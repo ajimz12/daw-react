3194375680659da3b806b8a6c2e763fe0241ca8e
ajimz12 <alvarojimgil12@gmail.com>
2025-01-13 16:02:36 +0100

    REFACTOR: Contador Doble

--- 01-inicio-react/src/components/ContadorDoble.jsx
+++ 01-inicio-react/src/components/ContadorDoble.jsx
@@ -16,7 +16,14 @@ function ContadorDoble() {
     });
   };
 
-  // TODO: Implementar el calculo de la media de los likes entre todos los amigos
+  const calculateAverage = () => {
+    const totalLikes = Object.values(friends).reduce(
+      (acc, curr) => acc + curr,
+      0
+    );
+    const totalFriends = Object.keys(friends).length;
+    return (totalLikes / totalFriends).toFixed(1);
+  };
 
   return (
     <div className="max-w-md mx-auto mt-8 p-6 bg-gray-200 shadow-md rounded-md">
@@ -60,6 +67,31 @@ function ContadorDoble() {
             Dislike
           </button>
         </div>
+      </div>
+      <div className="text-center mt-4">
+        <span>
+          Carlos tiene <strong>{friends.Carlos}</strong> likes
+        </span>
+        <div className="flex justify-center gap-4 mt-4">
+          <button
+            onClick={() => handleClickLike("Carlos", 1)}
+            className="bg-blue-500 hover:bg-blue-700 text-white rounded-md p-3"
+          >
+            Like
+          </button>
+          <button
+            onClick={() => handleClickLike("Carlos", -1)}
+            className="bg-red-500 hover:bg-red-700 text-white rounded-md p-3"
+          >
+            Dislike
+          </button>
+        </div>
+      </div>
+      <div className="text-center mt-8">
+        <h2 className="text-xl font-bold">Media de Likes</h2>
+        <p className="text-lg">
+          La media de likes es: <strong>{calculateAverage()}</strong>
+        </p>
       </div>
     </div>
   );
